d95f8aa005fe1650483ea3d4af36914de388604e
ShreyasMundhra <shreyass001@e.ntu.edu.sg>
2017-11-04 19:44:32 -0400

    further refactoring

--- featurizer.py
+++ featurizer.py
@@ -35,9 +35,24 @@ def writeDictToCsv(dict):
         for key in dict:
             file_object.write(key + ',' + str(dict[key]) + '\n')
 
+def writeTfIdfToCsv(data):
+    tf = vec(input='content', analyzer='word', min_df=0, stop_words='english', sublinear_tf=True, decode_error='ignore',
+             max_features=100)
+    tfidf_matrix = tf.fit_transform(data)
+    print("Features: " + str(tf.get_feature_names()))
+
+    df = pd.DataFrame(tfidf_matrix.toarray())
+    df.to_csv('tfidf.csv')
+    # with open('tfidf.csv', 'wb') as file_object:
+    #     for i in range(0, df.size):
+    #         file_object.write(str(df[i, 0]))
+    #         for j in range(1, df[0].size):
+    #             file_object.write(',' + str(df[i, j]))
+    #         file_object.write('\n')
+
 # create dictionary with the word as key and count as value for entire dataset to find important features for the model
-def get_word_dict():
-    word_count = {}
+def get_email_bodies():
+    # word_count = {}
     data = []
     for directory in os.listdir('train_data'):
         if(os.path.isdir('train_data/' + directory)):
@@ -47,18 +62,21 @@ def get_word_dict():
                 subject, body = get_sections(email)
 
                 data.append(' '.join(body))
-                for word in body:
-                    if(word in word_count):
-                        word_count[word] = word_count[word] + 1
-                    else:
-                        word_count[word] = 0
+                # for word in body:
+                #     if(word in word_count):
+                #         word_count[word] = word_count[word] + 1
+                #     else:
+                #         word_count[word] = 0
 
-    tf = vec(input='content', analyzer='word', min_df=0, stop_words='english', sublinear_tf=True, decode_error='ignore', max_features=100)
-    tfidf_matrix = tf.fit_transform(data)
-    print("Features: " + str(tf.get_feature_names()))
-
-    return word_count,tfidf_matrix
+    # tf = vec(input='content', analyzer='word', min_df=0, stop_words='english', sublinear_tf=True, decode_error='ignore', max_features=100)
+    # tfidf_matrix = tf.fit_transform(data)
+    # print("Features: " + str(tf.get_feature_names()))
+    # return word_count,tfidf_matrix
+    return data
 
 # readtxt('Summary.txt')
-word_dict,tfidf_matrix = get_word_dict()
-pd.DataFrame(tfidf_matrix.toarray()).to_csv("features.csv")
+# word_dict,tfidf_matrix = get_word_dict()
+data = get_email_bodies()
+# writeDictToCsv(word_dict)
+# writeMatrixToCsv(pd.DataFrame(tfidf_matrix.toarray()))
+writeTfIdfToCsv(data)
